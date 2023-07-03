ac4c143276a13d8c9992983f28b9f2680b67abc9
001 <den933@yandex.ru>
2023-07-03 08:07:42 +0300

    Задача 1

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,12 @@
+package jm.task.core.jdbc;
+
+import com.mysql.jdbc.Driver;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+
+
+
+    }
+}
